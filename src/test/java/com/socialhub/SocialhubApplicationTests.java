6b1464fca6b3586f090554e1de334737e2e6d7dc
sameeraherath <sameerastar6@gmail.com>
2025-11-05 10:59:36 +0530

    Initial commit

---
+++ src/test/java/com/socialhub/SocialhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
