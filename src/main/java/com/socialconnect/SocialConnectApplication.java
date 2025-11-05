2ff4afd92334c9e1276c65e6f2808355d14cd83b
sameeraherath <sameerastar6@gmail.com>
2025-11-05 11:15:37 +0530

    chore: update main application class to socialconnect package

---
+++ src/main/java/com/socialconnect/SocialConnectApplication.java
@@ -0,0 +1,14 @@
+package com.socialconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialConnectApplication.class, args);
+	}
+
+}
+
